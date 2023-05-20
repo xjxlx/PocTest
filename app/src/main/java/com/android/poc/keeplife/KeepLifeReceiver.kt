8dca79b57465a2e69654354b81354b2b6246a69c
xjxliuxing <xjxliuxing@163.com>
2023-05-20 21:56:54 +0800

    1: update log 2: broadCast

--- app/src/main/java/com/android/poc/keeplife/KeepLifeReceiver.kt
+++ app/src/main/java/com/android/poc/keeplife/KeepLifeReceiver.kt
@@ -3,29 +3,28 @@ package com.android.poc.keeplife
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.android.apphelper2.utils.LogUtil
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.launch
+import com.android.keeplife.BroadCastConstant
 
 class KeepLifeReceiver : BroadcastReceiver() {
 
-    private val mSharedFlow: MutableSharedFlow<String> by lazy {
-        return@lazy MutableSharedFlow()
-    }
-    private val mScope: CoroutineScope by lazy {
-        return@lazy CoroutineScope(Dispatchers.IO)
+    companion object {
+        private const val Type_Listener = "listener"
+        private const val Type_Close = "close"
+        private const val Type_Open = "open"
+        private const val Type_Error = "error"
     }
 
     override fun onReceive(context: Context, intent: Intent) {
-        mScope.launch {
-            mSharedFlow.debounce(5000)
-                .collect() {
-                    LogUtil.e("开始重新启动")
+        BroadCastConstant.init(context)
 
-                }
+        when (val type = intent.getStringExtra("type")) {
+            Type_Error -> { // 收到关闭的广播
+                BroadCastConstant.close(context)
+            }
+
+            Type_Listener -> {
+                BroadCastConstant.listener(type)
+            }
         }
     }
 }
